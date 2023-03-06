bbd93f46750a09126ed3add666fb13e1ce69a591
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-03-06 22:46:57 +0800

    add go.mod

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, Geektutu")
+		c.String(200, "Hello, Gin")
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
