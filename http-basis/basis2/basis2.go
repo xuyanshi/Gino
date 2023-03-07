dac1f6c99c24fb61169eaacd5c784adf99bea1cd
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-03-07 15:39:53 +0800

    Update basis2.go

--- http-basis/basis2/basis2.go
+++ http-basis/basis2/basis2.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Engine struct{}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("engine start")
+	switch req.URL.Path {
+	case "/":
+		_, err := fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+		if err != nil {
+			return
+		}
+	case "/hello":
+		for k, v := range req.Header {
+			_, err := fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+			if err != nil {
+				return
+			}
+		}
+	default:
+		_, err := fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+		if err != nil {
+			return
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	e := new(Engine)
+	log.Fatal(http.ListenAndServe(":8080", e))
 }
