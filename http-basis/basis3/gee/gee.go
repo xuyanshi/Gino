62630a71af3bfb8e1480a740ba1881e37a416b74
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-03-07 16:12:38 +0800

    add RUN and ServeHTTP methods.

--- http-basis/basis3/gee/gee.go
+++ http-basis/basis3/gee/gee.go
@@ -1,6 +1,9 @@
 package gee
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // HandlerFunc defines the request handler used by gee
 type HandlerFunc func(http.ResponseWriter, *http.Request)
@@ -14,6 +17,19 @@ func New() *Engine {
 	return &Engine{router: make(map[string]HandlerFunc)}
 }
 
-func main() {
+func (engine *Engine) RUN(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
 
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		_, err := fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+		if err != nil {
+			return
+		}
+	}
 }
