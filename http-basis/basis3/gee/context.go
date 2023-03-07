281082d0840bf8bedc1ae08c9ed724531ae96e10
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-03-07 21:36:29 +0800

    Update context.go

--- http-basis/basis3/gee/context.go
+++ http-basis/basis3/gee/context.go
@@ -1,6 +1,10 @@
 package gee
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type H map[string]interface{}
 
@@ -9,7 +13,7 @@ type Context struct {
 	Req        *http.Request
 	Path       string
 	Method     string
-	StatusCode int32
+	StatusCode int
 }
 
 func newContext(w http.ResponseWriter, req *http.Request) *Context {
@@ -23,4 +27,52 @@ func newContext(w http.ResponseWriter, req *http.Request) *Context {
 
 func (c *Context) PostForm(key string) string {
 	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	_, err := c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+	if err != nil {
+		return
+	}
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	_, err := c.Writer.Write(data)
+	if err != nil {
+		return
+	}
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	_, err := c.Writer.Write([]byte(html))
+	if err != nil {
+		return
+	}
 }
