cbec808245f9ca045baab54c362750af3bd74027
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-03-08 14:08:38 +0800

    Update router.go

--- http-basis/basis3/gee/router.go
+++ http-basis/basis3/gee/router.go
@@ -1,6 +1,9 @@
 package gee
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 type router struct {
 	handlers map[string]HandlerFunc
@@ -9,8 +12,18 @@ type router struct {
 func newRouter() *router {
 	return &router{handlers: make(map[string]HandlerFunc)}
 }
+
 func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
 	log.Printf("Route %4s - %s", method, pattern)
 	key := method + "-" + pattern
 	r.handlers[key] = handler
+}
+
+func (r *router) handle(c *Context) {
+	key := c.Method + "-" + c.Path
+	if handler, ok := r.handlers[key]; ok {
+		handler(c)
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+	}
 }
