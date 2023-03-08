13fde30214b30e4897ad76ca5216cde1a51bdd48
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-08 20:40:30 +0800

    Update trie.go

--- gee/trie.go
+++ gee/trie.go
@@ -1,5 +1,7 @@
 package gee
 
+import "strings"
+
 type node struct {
 	pattern  string
 	part     string
@@ -26,6 +28,13 @@ func (n *node) matchChildren(part string) []*node {
 	return nodes
 }
 
+/*
+插入功能递归查找每一层的节点，如果没有匹配到当前 part 的节点，则新建一个。
+需要注意，/p/:lang/doc 只有在第三层节点，即 doc 节点， pattern 才会设置为 /p/:lang/doc。
+p 和 :lang 节点的 pattern 属性均为空。
+因此，当匹配结束时，使用 n.pattern == "" 来判断路由规则是否匹配成功。
+例如，/p/python 虽能成功匹配到 :lang，但 :lang 的 pattern 值为空，因此匹配失败。
+*/
 func (n *node) insert(pattern string, parts []string, height int) {
 	if len(parts) == height {
 		n.pattern = pattern
@@ -35,12 +44,33 @@ func (n *node) insert(pattern string, parts []string, height int) {
 	part := parts[height]
 	child := n.matchChild(part)
 	if child == nil {
+		// Create a new node.
 		child = &node{part: part, isWild: part[0] == '*' || part[0] == ':'}
 		n.children = append(n.children, child)
 	}
 	child.insert(pattern, parts, height+1)
 }
 
-func (n *node) search(pattern string, height int) *node {
+/*
+查询功能，同样也是递归查询每一层的节点。
+退出规则是，匹配到了*，匹配失败，或者匹配到了第len(parts)层节点。
+*/
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(parts[height], "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+
+	part := parts[height]
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
 	return nil
 }
