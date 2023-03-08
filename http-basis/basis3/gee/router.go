d15683e0ce6e70af600290fb36c1d1d351ae9532
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-03-08 14:03:06 +0800

    Update router.go

--- http-basis/basis3/gee/router.go
+++ http-basis/basis3/gee/router.go
@@ -1,1 +1,14 @@
 package gee
+
+import "log"
+
+type router struct {
+	handlers map[string]HandlerFunc
+}
+
+func newRouter() *router {
+	return &router{handlers: make(map[string]HandlerFunc)}
+}
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	log.Printf("Route %4s - %s", method, pattern)
+}
