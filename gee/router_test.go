8c3e19935c69085a57792171ac31ddd8bf3a2ff9
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-08 23:11:21 +0800

    Update router_test.go

--- gee/router_test.go
+++ gee/router_test.go
@@ -46,19 +46,23 @@ func TestGetRoute2(t *testing.T) {
 	r.addRoute("GET", "/hi/:name", nil)
 	r.addRoute("GET", "/assets/*filepath", nil)
 
-	n1, ps1 := r.getRoute("GET", "/assets/file1.txt")
-	ok1 := n1.pattern == "/assets/*filepath" && ps1["filepath"] == "file1.txt"
-	if !ok1 {
-		t.Fatalf("pattern should be /assets/*filepath and filepath should be file1.txt.\n"+
-			"but now pattern is %s and filepath is %s.", n1.pattern, ps1["filepath"])
-	}
+	/*
+		PASSED TEST
+
+		n1, ps1 := r.getRoute("GET", "/assets/file1.txt")
+		ok1 := n1.pattern == "/assets/*filepath" && ps1["filepath"] == "file1.txt"
+		if !ok1 {
+			t.Fatalf("pattern should be /assets/*filepath and filepath should be file1.txt.\n"+
+				"but now pattern is %s and filepath is %s.", n1.pattern, ps1["filepath"])
+		}
+	*/
 
 	n2, ps2 := r.getRoute("GET", "/assets/css/test.css")
 	if n2 != nil && ps2 != nil {
 		ok2 := n2.pattern == "/assets/*filepath" && ps2["filepath"] == "css/test.css"
 		if !ok2 {
 			t.Fatalf("pattern should be /assets/*filepath & filepath should be css/test.css.\n"+
-				"but now pattern is %s and filepath is %s.", n1.pattern, ps1["filepath"])
+				"but now pattern is %s and filepath is %s.", n2.pattern, ps2["filepath"])
 		}
 	}
 	t.Fatal("r.getRoute(\"GET\", \"/assets/css/test.css\") returns nil")
