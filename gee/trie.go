e3fa7f6a59edb27777cf45762371b4314ea1535a
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-08 19:31:31 +0800

    matchChildren

--- gee/trie.go
+++ gee/trie.go
@@ -8,5 +8,20 @@ type node struct {
 }
 
 func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
 	return nil
+}
+
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
 }
