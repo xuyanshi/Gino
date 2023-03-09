90fb2ffaf8cf3c12fec5544ee6fe5ddf9bfa7df5
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-09 22:16:45 +0800

    Update recovery.go

--- gee/recovery.go
+++ gee/recovery.go
@@ -1,12 +1,22 @@
 package gee
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func trace(message string) string {
+	return ""
+}
 
 func Recovery() HandlerFunc {
 	return func(c *Context) {
 		defer func() {
 			if err := recover(); err != nil {
 				message := fmt.Sprintf("%s", err)
+				log.Printf("%s\n\n", message)
+				c.Fail(http.StatusInternalServerError, "Internal Server Error")
 			}
 		}()
 
