d3307646321c91491290b9a62a4f9f7918891c49
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-09 21:29:51 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"gee"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
 )
 
+type student struct {
+	Name string
+	age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	yy, mm, dd := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", yy, mm, dd)
+}
+
 func main() {
 	e := gee.New()
 	e.AddMiddleware(gee.Logger())
+	e.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	e.LoadHTMLGlob("templates/*")
 	e.Static("/assets", "./static")
 
+	stu1 := &student{
+		Name: "emo",
+		age:  23,
+	}
+	stu2 := &student{
+		Name: "maitian",
+		age:  24,
+	}
+	e.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	e.GET("/students", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
+			"title":   "gee",
+			"student": [2]*student{stu1, stu2},
+		})
+	})
 	e.Run(":9999")
 }
 
