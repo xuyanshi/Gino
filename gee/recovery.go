3652fe00ede586b929a3e82550368da34b779acb
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-09 22:07:31 +0800

    Update recovery.go

--- gee/recovery.go
+++ gee/recovery.go
@@ -1,9 +1,13 @@
 package gee
 
+import "fmt"
+
 func Recovery() HandlerFunc {
 	return func(c *Context) {
 		defer func() {
-
+			if err := recover(); err != nil {
+				message := fmt.Sprintf("%s", err)
+			}
 		}()
 
 		c.Next()
