541bb112ad416da2d546a183440b97a5805eaa38
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-09 10:26:31 +0800

    *RouterGroup directly

--- gee/gee.go
+++ gee/gee.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -15,29 +16,42 @@ type (
 		engine      *Engine
 	}
 	Engine struct {
-		routerGroup *RouterGroup
-		router      *router
-		groups      []*RouterGroup // store all groups here
+		*RouterGroup
+		router *router
+		groups []*RouterGroup // store all groups here
 	}
 )
 
 func New() *Engine {
 	engine := &Engine{router: newRouter()}
-	engine.routerGroup = &RouterGroup{engine: engine}
-	engine.groups = []*RouterGroup{engine.routerGroup}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
 	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
--- main.go
+++ main.go
@@ -2,34 +2,10 @@ package main
 
 import (
 	"gee"
-	"net/http"
 )
 
 func main() {
-	r := gee.New()
-
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello World</h1>")
-	})
-
-	r.GET("/hello", func(c *gee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-
-	r.GET("/hello/:name", func(c *gee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.POST("/login", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{
-			"username": "golang",
-			"password": "password123",
-		})
-	})
-
-	r.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
-	})
-
-	r.Run(":9999")
+	e := gee.New()
+	
+	e.Run(":9999")
 }
