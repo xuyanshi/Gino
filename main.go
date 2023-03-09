72f180d2f18f15ad7aff4a3c81a1fa660940e3ec
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-09 21:14:50 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,45 +12,7 @@ func main() {
 	e.AddMiddleware(gee.Logger())
 	e.Static("/assets", "./static")
 
-	//v1 := e.Group("/v1")
-	//{
-	//	v1.GET("/", func(c *gee.Context) {
-	//		c.HTML(http.StatusOK, "<h1>Hello</h1>")
-	//	})
-	//
-	//	v1.GET("/hello", func(c *gee.Context) {
-	//		// expect /hello?name=emo
-	//		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	//	})
-	//}
-
-	v2 := e.Group("/v2")
-	v2.AddMiddleware(middlewareV2())
-
-	{
-		v2.GET("/hello/:name", func(c *gee.Context) {
-			// expect /hello/emo
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-		})
-
-		// curl "http://localhost:9999/v2/login" -X POST -d "username=emo&password=emo123456"
-		//v2.POST("/login", func(c *gee.Context) {
-		//	c.JSON(http.StatusOK, gee.H{
-		//		"username": c.PostForm("username"),
-		//		"password": c.PostForm("password"),
-		//	})
-		//})
-	}
-
-	err := e.Run(":9999")
-	if err != nil {
-		return
-	}
-	// Useless
-	// cmd := exec.Command("curl http://localhost:9999/v2/hello/emo\n")
-	// cmd.Stdout = os.Stdout
-	// cmd.Stderr = os.Stderr
-	// cmd.Run()
+	e.Run(":9999")
 }
 
 func middlewareV2() gee.HandlerFunc {
