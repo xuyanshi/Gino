c0b6a093b0bc41533a72dce8ccad0aab2a180e98
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-09 15:03:46 +0800

    Update logger.go

--- gee/logger.go
+++ gee/logger.go
@@ -2,8 +2,15 @@
 
 package gee
 
+import (
+	"log"
+	"time"
+)
+
 func Logger() HandlerFunc {
 	return func(c *Context) {
-		
+		t := time.Now()
+		c.Next()
+		log.Printf("[%d] %s in %v", c.StatusCode, c.Req.RequestURI, time.Since(t))
 	}
 }
