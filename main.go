c29e85cf3c35cf053620ef977a85ab8ad212a05c
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-09 13:52:58 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"gee"
+	"net/http"
 )
 
 func main() {
 	e := gee.New()
-	
-	e.Run(":9999")
+
+	e.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+
+	v1 := e.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=emo
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := e.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/emo
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+		v2.POST("login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("emo"),
+				"password": c.PostForm("emo123456"),
+			})
+		})
+	}
+
+	err := e.Run(":9999")
+	if err != nil {
+		return
+	}
 }
